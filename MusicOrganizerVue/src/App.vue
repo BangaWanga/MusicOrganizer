a7df75d13d75189f2586eb8a7e6a4335dcf3a749
BangaWanga <nicolas.c.schilling@googlemail.com>
2024-07-27 20:00:03 +0200

    Added forgotten vue files

---
+++ MusicOrganizerVue/src/App.vue
@@ -0,0 +1,42 @@
+<style>
+
+.navbar {
+  overflow: hidden;
+  background-color: #333;
+  position: fixed; /* Set the navbar to fixed position */
+  top: 0; /* Position the navbar at the top of the page */
+  left: 0;
+  width: 100%; /* Full width */
+}
+.routerView{
+    margin-top: 5%;
+}
+.flexbox{
+    display: flex;
+   flex-direction: column;
+    left: 0
+}
+
+</style>
+
+<template>
+  <div id="app">
+    <div class="flexbox">
+      <div class="navbar">
+        <nav class="menu">
+          <RouterLink to="/">Go to Home</RouterLink>
+          <RouterLink to="/frontview">Go to Frontview</RouterLink>
+        </nav>
+      </div>
+<!--
+<router-view />
+-->
+      <div class="routerView">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
---
+++ MusicOrganizerVue/src/main.js
@@ -0,0 +1,53 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import { createMemoryHistory, createRouter } from 'vue-router'
+import Frontview from './components/Frontview.vue'
+import Projectview from './components/Projectview.vue'
+import AbletonProjectTable from './components/AbletonProjectTable.vue'
+//import HomeView from './Front.vue'
+
+
+const routes = [
+  { path: '/', component: AbletonProjectTable },
+  { path: '/frontview', component: Frontview, prope: true },
+  { path: '/project_view', component: Projectview, props: true  },
+]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
+//import router from './router/routes.js'
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
+
+
+
+
+import axios from 'axios'
+export default{
+    data()
+    {
+        return {
+            dataentry:{name:"",department:"",},
+        }
+	},
+    methods:{
+        get_projects:function(){
+            console.log("Trying to get something")
+            const path = 'http://127.0.0.1:5000/get_project_paths'
+            axios.get(path,
+                      {
+                          project_id: 0,
+                          //department:this.dataentry.department,
+                      }).then(response => {
+                          console.log(response)
+                      }).catch(err =>{console.log(err)
+                      })
+        },
+    }
+}
